5a4e66e7dc1bce77cc94002bc74dceb3e18fec85
eReR98 <abruderer7@gmail.com>
2024-02-06 12:47:29 -0700

    Assignment3 Completion

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,6 +13,8 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+export PATH=$PATH:/home/ajbru/Documents/InstallFiles/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin
+
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -39,33 +41,40 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # TODO: Add your kernel build steps here +
+    pwd
+    echo "compiling kernel"
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    #make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) modules
 
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 
 fi
 
 echo "Adding the Image in outdir"
 
-cp -a ${OUTDIR}/linux-stable/arch/arm64/boot/. ${OUTDIR}
+cp -a "${OUTDIR}/linux-stable/arch/arm64/boot/Image" "${OUTDIR}"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf "${OUTDIR}/rootfs"
 fi
 
-# TODO: Create necessary base directories
+# TODO: Create necessary base directories +
+mkdir rootfs
+cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -74,14 +83,16 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-make defconfig
-make CONFIG_PREFIX=${OUTDIR} install
-cd ..
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
@@ -89,13 +100,41 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 
+cp /home/ajbru/Documents/InstallFiles/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 "${OUTDIR}/rootfs/lib"
+cp /home/ajbru/Documents/InstallFiles/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 "${OUTDIR}/rootfs/lib64"
+cp /home/ajbru/Documents/InstallFiles/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 "${OUTDIR}/rootfs/lib64"
+cp /home/ajbru/Documents/InstallFiles/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 "${OUTDIR}/rootfs/lib64"
+
+
 # TODO: Make device nodes
 
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+
 # TODO: Clean and build the writer utility
+
+cd /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/writer "${OUTDIR}/rootfs/home"
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/finder.sh "${OUTDIR}/rootfs/home"
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/finder-test.sh "${OUTDIR}/rootfs/home"
+mkdir "${OUTDIR}/rootfs/home/conf"
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/conf/username.txt "${OUTDIR}/rootfs/home/conf"
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/conf/assignment.txt "${OUTDIR}/rootfs/home/conf"
+cp /home/ajbru/Documents/GitHub/LinuxSysProgramming/finder-app/autorun-qemu.sh "${OUTDIR}/rootfs/home"
+
 # TODO: Chown the root directory
 
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root *
+
 # TODO: Create initramfs.cpio.gz
+
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+gzip -f "${OUTDIR}/initramfs.cpio"
