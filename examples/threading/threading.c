dbbd18a075481f90b4fcc6867cea6b484ad14703
eReR98 <abruderer7@gmail.com>
2024-02-06 17:13:12 -0700

    Assignment4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,37 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data* t_data = (struct thread_data *) thread_param;
+
+    int rc = usleep(t_data->wait_to_obtain_ms * 1000.0);
+    if (rc != 0)
+    {
+        printf("Sleep error");
+        t_data->thread_complete_success=false;
+    }
+
+    rc = pthread_mutex_lock(t_data->timing_mutex);
+    if (rc != 0)
+    {
+        printf("Error obtaining mutex lock");
+        t_data->thread_complete_success=false;
+    }
+
+    rc = usleep(t_data->wait_to_release_ms * 1000.0);
+    if (rc != 0)
+    {
+        printf("Sleep error");
+        t_data->thread_complete_success=false;
+    }
+
+    rc = pthread_mutex_unlock(t_data->timing_mutex);
+    if (rc != 0)
+    {
+        printf("Error releasing mutex lock");
+        t_data->thread_complete_success=false;
+    }
+
     return thread_param;
 }
 
@@ -28,6 +59,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    // Borrowed this line from https://github.com/cu-ecen-aeld/assignments-3-and-later-Arkzeil/blob/main/examples/threading/threading.c
+    // To figure out correct syntax
+    struct thread_data* t_data = (struct thread_data *)malloc(sizeof(struct thread_data));
+
+    t_data->wait_to_obtain_ms=wait_to_obtain_ms;
+    t_data->wait_to_release_ms=wait_to_release_ms;
+    t_data->thread_complete_success=true;
+    t_data->timing_mutex=mutex;
+
+    int rc = pthread_create(thread, NULL, threadfunc, (void*)t_data);
+
+    if (rc != 0)
+    {
+        printf("Failed to create thread");
+        t_data->thread_complete_success=false;
+        return false;
+    }
+
+    return true;
 }
 
