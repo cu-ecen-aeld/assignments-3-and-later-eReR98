1eaf501c16d6720f9116f74a074aaaccac4cfabb
eReR98 <abruderer7@gmail.com>
2024-02-12 17:22:01 -0700

    Create aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# from lecture
+case "$1" in
+    start)
+        echo "Starting aesd-socket"
+        start-stop-daemon -S -n aesdsocket -d -a /usr/bin/aesdsocket
+        ;;
+    
+    stop)
+        echo "Stopping aesd-socket"
+        start-stop-daemon -K -n aesdsocket --signal SIGTERM
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
